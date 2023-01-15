3ddf565db2c2c3ff256d46e3ced07b2c8b47dcbb
jack9603301 <jack9603301@163.com>
2023-01-15 21:09:01 +0800

    support custom cert

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 #sysctl -w net.ipv4.ip_nonlocal_bind=1;
-./scripts/certificatssl.sh /usr/local/nginx/ssl-cert www &&\
+
+if [[ ! -z"${CUSTOM_CERT}" ]]; then
+./scripts/certificatssl.sh /usr/local/nginx/ssl-cert www
+else
+sed -i "s/\/usr\/local\/nginx\/ssl-cert\/{domain}.key/${CUSTOM_KEY_FILE}/g" /etc/bind/named.conf.options
+sed -i "s/\/usr\/local\/nginx\/ssl-cert\/{domain}\.crt/${CUSTOM_CERT_FILE}/g" /etc/bind/named.conf.options
+fi
+
 nohup python3.9 setup.py;
 tail -f /var/log/nginx/*.log;
+
 
 #iptables -t mangle -A PREROUTING -i eth0 -p tcp -m tcp --dport 80 -j TPROXY --on-ip 172.100.20.70 --on-port 7555 --tproxy-mark 1/1
 #iptables -t mangle -A PREROUTING -i eth0 --source 172.100.20.0/24 -j ACCEPT
